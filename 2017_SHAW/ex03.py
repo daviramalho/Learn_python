10943cbd65b238ceb68ac328a46cd3699290731e
Davi Ramalho <45436114+daviramalho@users.noreply.github.com>
2019-06-21 18:08:45 -0300

    Add files via upload

---
+++ 2017_SHAW/ex03.py
@@ -0,0 +1,36 @@
+#This will print some common text
+print("I will now count my chickens.")
+#Next will print some common text between the quotation mark.
+#The division will be made first than the addition, thats why the result will be
+#30.0
+#There is a ".0" after the result because of the division, that turned the number
+#into a floating number. Is it right? Not sure...
+print("Hens",25+30/6)
+#Next the result will be 97 because the order of the operations are:
+#1st."*"
+#2nd."%"
+#3rd."-"
+#Remember that "%" here means the rest of the division
+print("Roosters",100-25*3%4)
+#Next should only print common text again.
+print("Now I will count the eggs.")
+#Next the result should be 6.75 because the order of the operations are:
+#1st."%"
+#2nd."/"
+#Than the operations follow the order of appearance
+print(3+2+1-5+4%2-1/4+6)
+#Next should print common text
+print("Is it true that 3 + 2 < 5 - 7?")
+#Next should print false because the result is false...
+print(3+2<5-7)
+
+print("What is 3 + 2?",3+2)
+print("What is 5 - 7?",5-7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+#Next should print True or False if is is so...
+print("Is it greater?",5>-2)
+print("Is it greater or equal?",5>=-2)
+print("Is it less or equal?",5<=-2)
